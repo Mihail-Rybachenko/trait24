ecaad9cff79d98430da2c39cbdd9c70dd7de01f2
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-14 23:03:15 +0300

    tc_4_3

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/justice', function(req, res, next) {
-  res.send("<h1>Страница Справедливости</h1>")
+  res.render('trait', {
+    title: "Справедливость",
+    picture: "images/justice.webp",
+    desc: "это черта характера, которое отражает идею об отношении к людям и ситуациям справедливо и согласно закону или моральным нормам."
+    });
+    });
+router.get('/bravery', function(req, res, next) {
+res.render('trait', {
+title: "Храбрость",
+picture: "images/bravery.webp",
+desc: "Способность не бояться или преодолевать страх"
+});
  });
- router.get('/patience', function(req, res, next) {
-  res.send("<h1>Страница Терпения</h1>")
- });
- router.get('/bravery', function(req, res, next) {
-  res.send("<h1>Страница Храбрости</h1>")
- });
+router.get('/Patience', function(req, res, next) {
+res.render('trait', {
+title: "Терпение",
+picture: "images/Patience.webp",
+desc: "это способность выносить дискомфорт, физическое и психоэмоциональное напряжение, боль и прочие неудобства, не жалуясь и не противодействуя им."
+});
+});
 module.exports = router;
