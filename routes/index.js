c14979119ca04948bfbce09c930bf611717c5836
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-14 22:43:26 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get('/justice', function(req, res, next) {
+  res.send("<h1>Страница Справедливости</h1>")
+ });
+ router.get('/patience', function(req, res, next) {
+  res.send("<h1>Страница Терпения</h1>")
+ });
+ router.get('/bravery', function(req, res, next) {
+  res.send("<h1>Страница Храбрости</h1>")
+ });
 module.exports = router;
