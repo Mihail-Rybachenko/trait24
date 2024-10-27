5ec587b8060d480d6f039e2765f95849ac439d2f
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-10-27 14:45:17 +0200

    Mih_tr_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.uni = function(){
-    console.log(this.name + " мне помогла стать лучше")
-    }
-    
-const Trait = mongoose.model('Trait', schema);
-const character = new Trait({ name: 'Решительность' });
-character.save().then(() => character.uni());
+
+var Trait = require('./models/trait.js').Trait
+
+var trait = new Trait({
+    title: "Храбрость",
+    nick: "bravery",
+})
+trait.save();
