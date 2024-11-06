714281b79d97c7e7f58040432721d06f84ba9383
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-06 21:57:09 +0300

    Update branch 8_1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/tc2024')
+mongoose.connect('mongodb://localhost/Mih_tr2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
