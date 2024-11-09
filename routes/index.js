46d2cf97c913718e11128fddc7865432019db9ab
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-09 12:50:43 +0300

    Mih_tr_10_1

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ router.get('/', function(req, res, next) {
   req.session.greeting = "Hi!!!";
   res.render('index', { title: 'Express',  counter:req.session.counter  });
   });
-
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
 
 module.exports = router;
