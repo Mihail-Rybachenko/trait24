1ce468ef9c0ba6bd419359251a24bf62410fdadc
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-09 11:46:40 +0300

    Mih_tr_9_3

--- app.js
+++ app.js
@@ -22,12 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+var MongoStore = require('connect-mongo');
 app.use(session({
   secret: "CharacterTraits",
   cookie:{maxAge:60*1000},
   proxy: true,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/Mih_tr2024'})
   }))
   
 app.use('/', indexRouter);
