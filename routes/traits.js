562b660de02dbb7b039c84576cd5c121f9a5e1be
Mihail-Rybachenko <ribochenko.m.i.23@gmail.com>
2024-11-09 16:09:26 +0300

    Mih_tr_10_8

--- routes/traits.js
+++ routes/traits.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Trait = require('../models/trait').Trait;
-
+var checkAuth = require("../middlewares/checkAuth.js");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с traits');
 });
 /* Страница черт характера */
-router.get("/:nick", async function(req, res, next) {
+router.get("/:nick", checkAuth, async function(req, res, next) {
   var traits = await Trait.find({nick: req.params.nick});
   console.log(traits)
   if(!traits.length) return next(new Error("Нет такой черты в списке"))
